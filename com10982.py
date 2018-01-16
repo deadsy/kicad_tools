0f69ba68f2bd6c9d056864c232f9ee2bc28034a9
Jason Harris <sirmanlypowers@gmail.com>
2018-01-16 08:45:21 -0800

    Completed the rotary encoder footprint.

--- com10982.py
+++ com10982.py
@@ -20,8 +20,8 @@ url = 'https://www.sparkfun.com/products/10982'
 # (pin_number, pin_name, pin_type)
 pins = (
   ('A', 'PH_A', 'P'),
-  ('B', 'PH_B', 'P'),
   ('C', 'PH_Com', 'P'),
+  ('B', 'PH_B', 'P'),
   ('1', 'LED_R',  'P'),
   ('2', 'LED_G', 'P'),
   ('3', 'PB', 'P'),
@@ -48,7 +48,8 @@ lib.add_footprint('ggm', name)
 def pad_xy(pin_number):
   """return the pad x,y position"""
   if pin_number in ('A','B','C'):
-    x = ((ord(pin_number) - ord('A')) * 2.5) + 1.5
+    #x = ((ord(pin_number) - ord('A')) * 2.5) + 1.5
+    x = ({'A':0, 'C':1, 'B':2}[pin_number] * 2.5) + 1.5
     y = 14.5
   else:
     x = (int(pin_number) - 1) * 2.0
@@ -79,28 +80,43 @@ def mod_add_device_outline(mod):
   y = (14.5 - h) / 2.0
   mod.add_rect(x, y, w, h, 'F.Fab', 0.1)
 
+def mod_add_mounting_holes(mod):
+  hole_size = 2.0
+  pad_size = 3.0
+  d = 13.2
+  x0 = (8.0 - d) / 2.0
+  y0 = 7.0
+  lh = (x0, y0)
+  rh = (x0 + d, y0)
+  for (x, y) in (lh, rh):
+    p = kicad.mod_pad('', 'thru_hole', 'circle', ('*.Cu', '*.Mask'))
+    p.set_size(pad_size, pad_size).set_drill(hole_size)
+    p.set_xy(x, y)
+    mod.add_pad(p)
+
 def mod_add_text(mod):
   # silk reference
   t = kicad.mod_text('REF**', 'reference', 'F.SilkS')
-  t.set_xy(kicad.mil2mm(25), kicad.mil2mm(-100))
+  t.set_xy(-1.0, 15.0) # bottom left
   mod.add_shape(t)
   # silk pin 1
   t = kicad.mod_text('1', 'user', 'F.SilkS')
-  t.set_xy(kicad.mil2mm(-100), 0)
+  t.set_xy(-1.5, 0)
   mod.add_shape(t)
-  # fab name at top left
+  # fab name
   t = kicad.mod_text(name, 'value', 'F.Fab')
-  t.set_xy(kicad.mil2mm(-125), kicad.mil2mm(-1300))
+  t.set_xy(4.0, 7.0) # center
   mod.add_shape(t)
-  # fab reference at top left
+  # fab reference
   t = kicad.mod_text('%R', 'user', 'F.Fab')
-  t.set_xy(kicad.mil2mm(-125), kicad.mil2mm(-1200))
+  t.set_xy(-0.5,2.0) # top left
   mod.add_shape(t)
 
 def mod_init(mod):
   mod.add_tags(tags)
   mod_add_pads(mod, pins)
   mod_add_device_outline(mod)
+  mod_add_mounting_holes(mod)
   mod_add_text(mod)
 
 mod = kicad.mod_module(name, descr)
